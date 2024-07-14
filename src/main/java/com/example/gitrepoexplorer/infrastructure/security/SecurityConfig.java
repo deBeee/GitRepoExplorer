70943b3a06615080d996344f4e252d10292fe33b
deBeee <fiacik12345@gmail.com>
2024-07-14 16:10:44 +0200

    added own SecurityFilterChain

--- src/main/java/com/example/gitrepoexplorer/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/example/gitrepoexplorer/infrastructure/security/SecurityConfig.java
@@ -3,10 +3,16 @@ package com.example.gitrepoexplorer.infrastructure.security;
 import com.example.gitrepoexplorer.domain.user.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -19,5 +25,26 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs").permitAll()
+                        .requestMatchers("/swagger-resources/**").permitAll()
+                        .requestMatchers("/users/register/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/database/repo/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/database/repos").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/database/branch/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/github/repos/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 }
